34858d6724489846818ebbd44b9860c331fab28a
Phelipe Santos <phe2111@hotmail.com>
2023-10-09 18:24:43 -0300

    versão 0.2 finalizada

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var sec = 0
 var min = 0
+var hr = 0
 
 var interval 
 
@@ -8,13 +9,25 @@ function watch(){
     if(sec==60){
         min++
         sec=0
+        if (min==60) {
+            hr++
+            min=0
+        }
     }
-    document.getElementById('crono').innerText=min+':'+sec
+    document.getElementById('crono').innerText= decimal(hr)+ ':' + decimal(min) + ':' + decimal(sec)
+}
+
+function decimal(digito) {
+    if(digito<10){
+        return('0'+digito)
+    }else{
+        return(digito)
+    }
 }
 
 function start() {
     watch()
-    interval=setInterval(watch,100)
+    interval=setInterval(watch,1000)
 }
 function pause() {
    clearInterval(interval)
@@ -23,5 +36,5 @@ function stop() {
     clearInterval(interval)
     sec=0
     min=0
-    document.getElementById('crono').innerText='00:00'
+    document.getElementById('crono').innerText='00:00:00'
 }
